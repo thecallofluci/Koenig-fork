53ee7d76b52cc486f418a2c47e336e3200aef283
PLAYER 251 <callofluci@pm.me>
2023-09-02 17:10:55 -0700

    Update DarkModeToggle.jsx

--- packages/koenig-lexical/demo/components/DarkModeToggle.jsx
+++ packages/koenig-lexical/demo/components/DarkModeToggle.jsx
@@ -2,7 +2,7 @@ const DarkModeToggle = ({darkMode, toggleDarkMode}) => {
     return (
         <>
             <button className="absolute right-20 top-4 z-20 block h-[22px] w-[42px] cursor-pointer rounded-full transition-all ease-in-out" type="button" onClick={toggleDarkMode}>
-                {darkMode ? '🌚' : '🌞'}
+                {darkMode ? '"dark"' : '"light"'}
             </button>
         </>
     );
